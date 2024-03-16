7780565cdb0debb84564a3fbad69d88067265f4c
Dale <xiangkeguo@gmail.com>
2024-03-15 20:06:29 -0700

    udpate

--- app/Services/Resources/Wallstreetcn.php
+++ app/Services/Resources/Wallstreetcn.php
@@ -16,6 +16,7 @@ final class Wallstreetcn{
             $cacheKey = "xbot.keyword.Wallstreetcn";
             $data = Cache::store('redis')->get($cacheKey, false);
             if(!$data){
+            // if(1){
                 $response = Http::get("https://api-one-wscn.awtmt.com/apiv1/search/article?query=华尔街见闻早餐&cursor=&limit=1&vip_type=");
                 $json =$response->json();
                 $id = $json['data']['items'][0]['id'];
@@ -28,14 +29,14 @@ final class Wallstreetcn{
                 $html = $json['data']['content'];
 
                 $htmlTmp = HtmlDomParser::str_get_html($html);
-                $mp3 =  $htmlTmp->findOne('img.wscnph')->getAttribute('data-uri');
+                $mp3 =  $htmlTmp->findOne('img.editor-placeholder')->getAttribute('data-uri');
 
                 $title = "华尔街见闻早餐:{$date}";
 				$desc = "市场有风险，投资需谨慎。本文不构成个人投资建议";
                 $data =[
-                    "url" => $mp3,
+                    "url" => $mp3,//$json['data']['audio_uri'],
                     'title' => $title,
-                    'description' => $desc,
+                    'description' => $json['data']['audio']['title'] . $desc,
                 ];
                 Cache::store('redis')->put($cacheKey, $data, strtotime('tomorrow') - time());
             }
